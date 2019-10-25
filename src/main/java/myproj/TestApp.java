99ee4b2ca2d31ffdb7eda05ce68fdf0655af248e
ravikanttripathi19 <57004757+ravikanttripathi19@users.noreply.github.com>
2019-10-25 19:00:37 +0530

    Update TestApp.java

--- src/main/java/myproj/TestApp.java
+++ src/main/java/myproj/TestApp.java
@@ -5,8 +5,8 @@ public class TestApp {
 	public static void main(String[] args) {
      int id=10;
      System.out.println("Hi This Is Ravikant "+id);
-		System.out.println(Ravikant);
-		tripathi ji
+		System.out.println("Ravikant");
+		System.out.println("ji");
 	}
 
 }
